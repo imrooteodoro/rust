58d71b552dc476d73401fbce8e3856a57985c520
imrooteodoro <adelsonteodoro67@gmail.com>
2024-05-16 21:28:37 -0300

    feat:update vector file

--- exerc/vector_folder/vector.rs
+++ exerc/vector_folder/vector.rs
@@ -1,15 +1,21 @@
+
 fn main(){
     vec_func();
+
+   
 }
 
 fn vec_func(){
-    let mut i = vec![0, 5, 150];
-    let mut j = vec![0, 10, 100];
-    let mut iterator = 0;
-    let mut result = 0; 
-    while iterator <  i.len(){
-         result = i[iterator] * j [iterator];
-         iterator+=1;
+    let i = vec![5, 10, 5, 7, 6];
+    let mut result = vec![] ;
+    let mut iterator = 0; 
+    while iterator < i.len() {
+       if i[iterator] % 2 != 0{
+        result = vec![i[iterator]];
+        
+       }
+
+       iterator+=1;
     } 
     println!("{:?}", result);
 }
